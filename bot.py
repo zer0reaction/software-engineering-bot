39895b46b897e0a6eccc5c94cd52e8795f6e1672
Nikita Rudakov <boltgopnik@gmail.com>
2024-09-17 20:14:18 +0300

    Basic event test

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import telebot
 import creds
+import msg
 from telebot import types
 
 
@@ -8,51 +9,86 @@ bot = telebot.TeleBot(creds.bot_token)
 
 # Initial message
 def hello(message):
-    markup = types.InlineKeyboardMarkup()  # Создаем разметку для inline-кнопок
+    markup = types.InlineKeyboardMarkup()
     button1 = types.InlineKeyboardButton("День 1", callback_data='day_1')
     button2 = types.InlineKeyboardButton("День 2", callback_data='day_2')
     button3 = types.InlineKeyboardButton("День 3", callback_data='day_3')
 
-    markup.add(button1, button2, button3)  # Добавляем кнопки в разметку
+    markup.add(button1, button2, button3)
 
-    bot.send_message(message.chat.id, "Привет! Выберите день:", reply_markup=markup)
+    bot.send_message(message.chat.id, msg.greeting, reply_markup=markup)
 
 
+# ----- DAY 1 -----
 def day_1_display(call):
-    markup = types.InlineKeyboardMarkup()  # Создаем разметку для inline-кнопок
-    button1 = types.InlineKeyboardButton("Назад", callback_data='hello')
-    markup.add(button1)  # Добавляем кнопки в разметку
+    markup = types.InlineKeyboardMarkup()
+    back_button = types.InlineKeyboardButton("Назад", callback_data='hello')
+    event_1_button = types.InlineKeyboardButton(msg.day_1_event_1_label, callback_data='day_1_event_1')
 
-    bot.send_photo(call.message.chat.id, photo=open("img/test.jpg", "rb"), caption="kek", reply_markup=markup)
+    markup.add(event_1_button, back_button)
+
+    bot.send_photo(call.message.chat.id, photo=open("img/test.jpg", "rb"), caption=msg.day_1_text, reply_markup=markup)
+
+def day_1_event_1_display(call):
+    markup = types.InlineKeyboardMarkup()
+    back_button = types.InlineKeyboardButton("Назад", callback_data='day_1')
+
+    markup.add(back_button)
+
+    bot.send_photo(call.message.chat.id, photo=open("img/test.jpg", "rb"), caption=msg.day_1_event_1_text, reply_markup=markup)
+# -----------------
 
 
+# ----- DAY 2 -----
 def day_2_display(call):
-    markup = types.InlineKeyboardMarkup()  # Создаем разметку для inline-кнопок
-    button1 = types.InlineKeyboardButton("Назад", callback_data='hello')
-    markup.add(button1)  # Добавляем кнопки в разметку
+    markup = types.InlineKeyboardMarkup()
+    event_1_button = types.InlineKeyboardButton(msg.day_2_event_1_label, callback_data='day_2_event_1')
+    back_button = types.InlineKeyboardButton("Назад", callback_data='hello')
 
-    bot.send_message(call.message.chat.id, "Template day 2")
+    markup.add(event_1_button, back_button)
+
+    bot.send_photo(call.message.chat.id, photo=open("img/test.jpg", "rb"), caption=msg.day_2_text, reply_markup=markup)
+
+def day_2_event_1_display(call):
+    markup = types.InlineKeyboardMarkup()
+    back_button = types.InlineKeyboardButton("Назад", callback_data='day_2')
+
+    markup.add(back_button)
+
+    bot.send_photo(call.message.chat.id, photo=open("img/test.jpg", "rb"), caption=msg.day_2_event_1_text, reply_markup=markup)
+# -----------------
 
 
+# ----- DAY 3 -----
 def day_3_display(call):
     markup = types.InlineKeyboardMarkup()  # Создаем разметку для inline-кнопок
-    button1 = types.InlineKeyboardButton("Назад", callback_data='hello')
-    markup.add(button1)  # Добавляем кнопки в разметку
+    event_1_button = types.InlineKeyboardButton(msg.day_3_event_1_label, callback_data='day_3_event_1')
+    back_button = types.InlineKeyboardButton("Назад", callback_data='hello')
 
-    bot.send_message(call.message.chat.id, "Template day 3")
+    markup.add(event_1_button, back_button)
+
+    bot.send_photo(call.message.chat.id, photo=open("img/test.jpg", "rb"), caption=msg.day_3_text, reply_markup=markup)
+
+def day_3_event_1_display(call):
+    markup = types.InlineKeyboardMarkup()
+    back_button = types.InlineKeyboardButton("Назад", callback_data='day_3')
+
+    markup.add(back_button)
+
+    bot.send_photo(call.message.chat.id, photo=open("img/test.jpg", "rb"), caption=msg.day_3_event_1_text, reply_markup=markup)
+# -----------------
 
 
-# Обработчик команды /start
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     hello(message)
 
 
-# Обработчик нажатий на inline-кнопки
 @bot.callback_query_handler(func=lambda call: True)
 def callback_inline(call):
     if call.data == 'hello':
         hello(call.message)
+
     elif call.data == 'day_1':
         day_1_display(call)
     elif call.data == 'day_2':
@@ -60,6 +96,12 @@ def callback_inline(call):
     elif call.data == 'day_3':
         day_3_display(call)
 
+    elif call.data == 'day_1_event_1':
+        day_1_event_1_display(call)
+    elif call.data == 'day_2_event_1':
+        day_2_event_1_display(call)
+    elif call.data == 'day_3_event_1':
+        day_3_event_1_display(call)
 
-# Запуск бота
+
 bot.polling(none_stop=True)
