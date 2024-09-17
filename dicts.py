fc493a8dd8473a666f9d2db4d4c36732d9dabe6e
Nikita Rudakov <boltgopnik@gmail.com>
2024-09-17 23:40:29 +0300

    Added day 1 images

--- dicts.py
+++ dicts.py
@@ -1,14 +1,29 @@
 days = {
     "day_1": {
         "text": "Sample day 1 text",
-        "image": "img/test.jpg",
+        "image": "img/day_1_main.jpg",
         "label": "День 1",
 
         "events": {
             "day_1_event_1": {
-                "label": "Day 1 event 1 label",
+                "label": "Набережная Северной Двины",
                 "text": "Day 1 event 1 text",
-                "image": "img/test.jpg"
+                "image": "img/naberezhnaya_dvini.jpg"
+            },
+            "day_1_event_2": {
+                "label": "Памятник Петру I",
+                "text": "Day 1 event 2 text",
+                "image": "img/petr_1.jpg"
+            },
+            "day_1_event_3": {
+                "label": "Мыс Пур-Наволок",
+                "text": "Day 1 event 3 text",
+                "image": "img/pur_navolok.jpg"
+            },
+            "day_1_event_4": {
+                "label": "Успенская Церковь",
+                "text": "Day 1 event 4 text",
+                "image": "img/uspenskaya_cerkov.jpg"
             }
         }
     }
